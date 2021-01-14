389d7629b17e831c3111cc98206accdc1f79c96d
Desenvolvedor 5 <desenvolvedor5@desenvolvedor5.com.br>
2021-01-14 16:01:25 -0300

    Adicionado o MidasLib no form pricipal para não dar erro ao criar um ClientDataSet

--- src/Main.pas
+++ src/Main.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, MidasLib;
 
 type
   TfMain = class(TForm)
@@ -13,6 +13,7 @@ type
     btStreams: TButton;
     procedure btDatasetLoopClick(Sender: TObject);
     procedure btStreamsClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
   public
   end;
@@ -35,6 +36,11 @@ end;
 procedure TfMain.btStreamsClick(Sender: TObject);
 begin
   fClienteServidor.Show;
+end;
+
+procedure TfMain.FormShow(Sender: TObject);
+begin
+  ReportMemoryLeaksOnShutdown := True;
 end;
 
 end.
